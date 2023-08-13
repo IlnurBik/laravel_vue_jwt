2832fac818aabf5678451b1e63df5b0514805a27
IlnurBik <biktimerovilnur@yandex.ru>
2023-08-13 23:43:54 +0500

    Добавление правил в router.beforeEach

---
+++ resources/js/components/404.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="d-flex align-items-center justify-content-center vh-100 bg-primary">
+        <h1 class="display-1 fw-bold text-white">404</h1>
+    </div>
+</template>
+
+<script>
+export default {
+name: "404"
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/router.js
+++ resources/js/router.js
@@ -17,9 +17,14 @@ const routes = [
         name: 'user.person',
     },
     {
-        path: '/cars',
+        path: '/',
         component: () => import('./components/Car/Index.vue'),
         name: 'cars.index'
+    },
+    {
+        path: '/:pathMatch(.*)',
+        component: () => import('./components/404.vue'),
+        name: '404'
     },
 ];
 
@@ -27,6 +32,24 @@ const router = createRouter({
     history: createWebHistory(),
     routes,
     linkExactActiveClass: 'active',
+});
+
+router.beforeEach((to, from, next)=>{
+    const token = localStorage.getItem('access_token');
+
+    if(token){
+        if(to.name === 'user.login' || to.name === 'user.registration'){
+            return next({name: 'user.person'})
+        }
+    }
+    if(!token){
+        if(to.name === 'user.person' || to.name === 'cars.index'){
+            return next({name: 'user.login'});
+        }
+    }
+
+    return next();
+
 })
 
 export default router
